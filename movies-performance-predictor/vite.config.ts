dd3f25d95fa7821168f6094e94f57a3eb865cbff
Addala Raed <addala.raed@gmail.com>
2025-02-09 11:02:45 +0100

    setup config

--- movies-performance-predictor/vite.config.ts
+++ movies-performance-predictor/vite.config.ts
@@ -3,6 +3,40 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+// import viteCompression from 'vite-plugin-compression';
+// import { fileURLToPath } from "url";
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(),],
 });
+
+// Build Configuration with more improvements
+// export default defineConfig({
+//   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(),
+//   viteCompression({
+//     algorithm: 'brotliCompress',
+//     ext: '.br',
+//     threshold: 1025,
+//     deleteOriginFile: true,
+//   }),
+//   ],
+//   resolve: {
+//     alias: [
+//       {
+//         find: "@",
+//         replacement: fileURLToPath(new URL("./app", import.meta.url)),
+//       },
+//     ],
+//   },
+//   build: {
+//     sourcemap: 'hidden',
+//     cssCodeSplit: true,
+//     cssMinify: 'lightningcss',
+//     minify: "terser",
+//     terserOptions: {
+//       format: {
+//         comments: false,
+//       },
+//     },
+//   },
+// });
